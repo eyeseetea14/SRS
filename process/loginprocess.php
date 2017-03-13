82788d124074547f0eea117fc69a474905529e76
sanzillion <sanzimagery@gmail.com>
2017-03-13 20:20:41 +0800

    this change

--- process/loginprocess.php
+++ process/loginprocess.php
@@ -22,7 +22,6 @@ if($db = connect()){
 
 				$query->execute($execute_query);
 
-				if($user == "admin" || $user == "admin2" || $user == "dean"){
 					$stmt2 = $db->prepare("SELECT * from admin where user = :user");
 					$stmt2->bindValue(':user',$user);
 					$stmt2->execute();
@@ -31,11 +30,6 @@ if($db = connect()){
 					$_SESSION['admin']=$user;
 					$_SESSION['id']=$id;
 					header("Location:../pages/index.php");
-				}
-				else{
-					header("Location:../index.php?error=1");
-				}
-
 		}
 		else{
 			header("Location:../index.php?error=1");
